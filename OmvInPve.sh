77fbbcb4e9cdb95f7f981495171268a71c045ddf
root <root@pve.example.com>
2019-04-23 12:48:22 +0800

    1.0

--- OmvInPve.sh
+++ OmvInPve.sh
@@ -3,12 +3,15 @@ OS=`/usr/bin/pveversion|awk -F'-' 'NR==1{print $1}'`
 ver=`/usr/bin/pveversion|awk -F'/' 'NR==1{print $2}'|awk -F'-' '{print $1}'`
 pve=$OS$ver
 if [ "$OS" != "pve" ];then
+    echo "您的系统不是Proxmox VE, 无法安装，马上退出!"
     echo "Your OS is not Proxmox VE.Now quit!"
     exit
 fi
 while [ true ]               
   do
+     echo "如果您使用的是zfs文件系统，请务必给rpool做个快照，如果有问题可以回滚快照后重启系统!"
      echo "If you use zfs rpool for your system, please make sure your rpool has made a snapshot!"
+     echo "您的系统是：$pve, 您将安装OMV4, 是否继续?(y/n)"
      echo -n "Your OS：$pve, you will install OMV4, continue?(y/n)"
      read x
      case "$x" in
@@ -59,7 +62,7 @@ while [ true ]
 		  nginx pm-utils wpasupplicant samba-vfs-modules python3-pyudev python3-natsort jq ntp python3-netifaces python3-lxml resolvconf
 	elif [ $ver = "5.4" ];then
 		apt-get autoremove openmediavault
-		apt-get -f install apparmor attr bridge-utils ceph-common ceph-fuse cifs-utils corosync criu cstream dtach ebtables \
+		apt-get -f -y install apparmor attr bridge-utils ceph-common ceph-fuse cifs-utils corosync criu cstream dtach ebtables \
 		  faketime file fonts-font-awesome genisoimage glusterfs-client glusterfs-common ipset libacl1-dev \
 		  libanyevent-http-perl libanyevent-perl libapparmor-perl libappconfig-perl libapt-pkg-perl libarchive13 \
 		  libasound2 libasound2-data libattr1-dev libauthen-pam-perl libbabeltrace-ctf1 libbabeltrace1 \
@@ -91,7 +94,7 @@ while [ true ]
 		  python-rados python-rbd python-requests python-six python-urllib3 qemu-server smbclient spiceterm \
 		  sqlite3 thin-provisioning-tools uidmap vncterm xsltproc xz-utils sudo ntfs-3g python3-dbus 
 
-		  apt-get -f install libjs-extjs6 php-fpm php-json php-cgi php-cli php-mbstring php-pam ethtool python3-dialog acl jfsutils \
+		  apt-get -f -y install libjs-extjs6 php-fpm php-json php-cgi php-cli php-mbstring php-pam ethtool python3-dialog acl jfsutils \
 		  sdparm mdadm libsasl2-modules cpufrequtils uuid nfs-kernel-server proftpd-basic lsb-release sshpass samba \
 		  samba-common-bin wsdd avahi-daemon libnss-mdns monit acpid beep php-bcmath rrdtool collectd anacron cron-apt \
 		  quota php-xml quotatool liblocale-po-perl proftpd-mod-vroot libjavascript-minifier-xs-perl xmlstarlet parted \
@@ -106,9 +109,11 @@ while [ true ]
 	sed -i "s/watchdog, //g" omvtmp/DEBIAN/control 
 	dpkg -b omvtmp openmediavault_4.1.22-1_all.deb 
 	dpkg --force-all -i openmediavault_4.1.22-1_all.deb
+        echo "安装完成，下面初始化OMV!"
         echo "Installation Complete, now init the OMV!"
 	rm -rf ./omvtmp
 	omv-initsystem
+        echo "如果没有意外，安装完成! 浏览器打开http://ip 去试试您的OMV!"
         echo "Installation Complete! Go to http://ip to enjoy OMV!"
         exit
         ;;
