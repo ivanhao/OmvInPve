f6308d3725552216cdd9b350a72f546485e5cde6
ivanhao <ivanhao1984@qq.com>
2019-12-04 15:55:46 +0800

    pve6 support

--- OmvInPve.sh
+++ OmvInPve.sh
@@ -141,7 +141,7 @@ while [ true ]
                 echo -n "Your OS：$pve, you will install OMV5"
                 cat <<EOF > /etc/apt/sources.list.d/openmediavault.list
 #deb http://packages.openmediavault.org/public arrakis main
-deb https://package.openmediavault.org/public usul main
+deb https://packages.openmediavault.org/public usul main
 EOF
                 wget -O "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc" https://packages.openmediavault.org/public/archive.key
                 apt-key add "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc"
