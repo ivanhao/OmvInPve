febdc96e0b7a2cf93055070823b0f1d26a096f62
ivanhao <ivanhao1984@qq.com>
2019-12-09 13:12:35 +0800

    pve6 support

--- OmvInPve.sh
+++ OmvInPve.sh
@@ -148,7 +148,7 @@ pve60(){
 deb https://packages.openmediavault.org/public usul main
 EOF
     apt-mark unhold openmediavault
-    apt-get autoremove openmediavault
+    apt-get -y autoremove openmediavault
     wget -O "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc" https://packages.openmediavault.org/public/archive.key
     apt-key add "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc"
     apt-get update
