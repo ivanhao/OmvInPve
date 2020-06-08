9f589ff97fc82e534178473dd4bcc67d68ef3480
ivanhao <ivanhao1984@qq.com>
2020-06-08 17:55:07 +0800

    systemd-v1.0

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+basedir=`cd $(dirname $0); pwd -P`
+#url="https://www.pvenas.com/pvenas.tar.gz"
+url="https://drive.pvenas.com/api/public/dl/Umf2kq7P/omvInPve.tar.bz2"
+config(){
+		echo "downloading bin ..."
+		wget -c --no-check-certificate --timeout 15 --waitretry 5 --tries 5 $url 
+		echo "downloaded"
+        tar -xvjf omvInPve.tar.bz2
+        cd omvInPve
+        chmod +x ./*.sh
+        ./install.sh
+}
+
+config
