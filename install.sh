581af118c5d73c6a2ccab549d0f859ec0d57b95e
ivanhao <ivanhao1984@qq.com>
2020-06-08 17:58:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ basedir=`cd $(dirname $0); pwd -P`
 #url="https://www.pvenas.com/pvenas.tar.gz"
 url="https://drive.pvenas.com/api/public/dl/Umf2kq7P/omvInPve.tar.bz2"
 config(){
-		echo "downloading bin ..."
-		wget -c --no-check-certificate --timeout 15 --waitretry 5 --tries 5 $url 
-		echo "downloaded"
+	echo "downloading bin ..."
+	wget -c --no-check-certificate --timeout 15 --waitretry 5 --tries 5 $url 
+	echo "downloaded"
         tar -xvjf omvInPve.tar.bz2
         cd omvInPve
         chmod +x ./*.sh
