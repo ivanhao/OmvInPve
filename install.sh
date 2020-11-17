3480ebd4de76cd2eccb655f127116315c8d67175
ivanhao <ivanhao1984@qq.com>
2020-11-17 16:17:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 basedir=`cd $(dirname $0); pwd -P`
 #url="https://www.pvenas.com/pvenas.tar.gz"
-url="https://drive.pvenas.com/api/public/dl/jQ1_ljuj/omvInPve.tar.bz2"
+#url="https://drive.pvenas.com/api/public/dl/jQ1_ljuj/omvInPve.tar.bz2"
+url1="http://i.pvenas.com:58080/api/public/dl/24ofof_g/omvInPve.tar.bz2"
+url2="http://j.pvenas.com:58080/api/public/dl/24ofof_g/omvInPve.tar.bz2"
+url3="http://k.pvenas.com:58080/api/public/dl/24ofof_g/omvInPve.tar.bz2"
+url4="http://l.pvenas.com:58080/api/public/dl/24ofof_g/omvInPve.tar.bz2"
 config(){
+	apt install -y axel
 	echo "downloading bin ..."
-	wget -c --no-check-certificate --timeout 15 --waitretry 5 --tries 5 $url 
+	axel -n 3 -s 3096000 -o ./omvInPve.tar.bz2 $url1 $url2 $url3 $url4 
+	#wget -c --no-check-certificate --timeout 15 --waitretry 5 --tries 5 $url 
 	echo "downloaded"
         tar -xjf omvInPve.tar.bz2
         cd omvInPve
